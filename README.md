d321fe3868f88940d3d0bd21af4c897ff2ccd7c5
Matt Le <ml9951@rit.edu>
2016-05-04 14:48:48 -0400

    updating docs

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This repo contains a copy of the ICFP Coq proofs, modified to model TinySTM.
 
 The entire development can be built by running `make`
 
+Documentation can be found [here](https://rawgit.com/ml9951/ICFP15-Coq-Proofs/JFP-Proofs/docs/index.html)
--- gendocs.sh
+++ gendocs.sh
@@ -3,3 +3,7 @@
 make 
 coqdoc *.v -d docs/
 sed -i '' 's/ICFP\.//g' docs/index.html
+sed -i '' 's/[a-zA-Z]*\.html/https:\/\/rawgit.com\/ml9951\/ICFP15-Coq-Proofs\/JFP-Proofs\/docs\/&/g' docs/index.html
+
+
+#add `https://rawgit.com/ml9951/ICFP15-Coq-Proofs/JFP-Proofs/docs/` on to every URL
